fc8c3e6b57327f30ebee5717b0e544b75b66d75c
Matthew Makai <makaimc@gmail.com>
2012-01-06 10:22:35 -0500

    Updating code with requests instead of httplib.

--- urbanairship.py
+++ urbanairship.py
@@ -1,7 +1,7 @@
 """Python module for using the Urban Airship API"""
 
-import httplib
 import urllib
+import requests
 try:
     import json
 except ImportError:
@@ -83,18 +83,24 @@ class Airship(object):
         self.auth_string = ('%s:%s' % (key, secret)).encode('base64')[:-1]
 
     def _request(self, method, body, url, content_type=None):
-        h = httplib.HTTPSConnection(SERVER)
-        headers = {
-            'authorization': 'Basic %s' % self.auth_string,
-        }
-        if content_type:
-            headers['content-type'] = content_type
-        h.request(method, url, body=body, headers=headers)
-        resp = h.getresponse()
-        if resp.status == 401:
-            raise Unauthorized
+        #h = httplib.HTTPSConnection(SERVER)
+        #headers = {
+        #    'authorization': 'Basic %s' % self.auth_string,
+        #}
+        #if content_type:
+        #    headers['content-type'] = content_type
+        #h.request(method, url, body=body, headers=headers)
+        #resp = h.getresponse()
+        #if resp.status == 401:
+        #    raise Unauthorized
+        #return resp.status, resp.read()
 
-        return resp.status, resp.read()
+        headers = {'content-type': content_type}
+        auth = (self.key, self.secret)
+        r = requests.request(method, url, data=body, auth=auth, headers=headers)
+        if r.status_code == 401:
+            raise Unauthorized
+        return r.status_code, r.content
 
     def register(self, device_token, alias=None, tags=None, badge=None):
         """Register the device token with UA."""
