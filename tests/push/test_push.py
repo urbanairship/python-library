303fea167d9702c06efe003e96f682f86a438e71
Adam Lowry <adam@therobots.org>
2013-06-20 11:19:25 -0700

    Fix test

--- tests/push/test_push.py
+++ tests/push/test_push.py
@@ -9,7 +9,8 @@ class TestPush(unittest.TestCase):
 
     def test_push_success(self):
         with mock.patch.object(ua.Airship, '_request') as mock_request:
-            mock_request.return_value = (202, 'OK')
+            mock_request.return_value = (202,
+                '''{"push_ids": ["0492662a-1b52-4343-a1f9-c6b0c72931c0"]}''')
 
             airship = ua.Airship('key', 'secret')
             push = airship.create_push()
