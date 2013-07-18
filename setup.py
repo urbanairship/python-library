2a374e6130e355b5f2e7456e826fe754649dd51a
Adam Lowry <adam@therobots.org>
2013-07-18 14:22:54 -0700

    Let's mark this as 0.6 beta

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name="urbanairship",
-    version="0.6",
+    version="0.6b",
     author="Adam Lowry",
     author_email="adam@urbanairship.com",
     url="http://urbanairship.com/",
