e89ca4732f9ece16238d890b305af1ced728cb3e
Alexey Kinyov <rudy@05bit.com>
2013-12-23 14:26:08 +0400

    add Python version classifiers to setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,8 @@ setup(
     packages=["urbanairship", "urbanairship.push", "urbanairship.devices"],
     license='BSD License',
     classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
