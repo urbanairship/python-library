d339b9a62f771262ffaf107b1d080a512a76b687
Lisa Hewus Fresh <l.hewus-fresh@urbanairship.com>
2014-12-22 14:57:55 -0800

    Ticket CE-758 - Fixed formatting in tests

--- tests/devices/test_device_info.py
+++ tests/devices/test_device_info.py
@@ -80,16 +80,22 @@ class TestDeviceInfo(unittest.TestCase):
     def test_device_token_feedback(self):
         with mock.patch.object(ua.Airship, "_request") as mock_request:
             response = requests.Response()
-            response._content = ('''[{
-                                   "alias" : null,
-                                   "device_token" : "4D95CB349F95ADEBE05F0A71B5F5B62D0F696667454DA60CD55282F67321710C",
-                                   "marked_inactive_on" : "2014-12-16 20:21:42"
-                                 }]''')
+            response._content = (
+                '''[{
+                    "alias" : null,
+                    "device_token" : "4D95CB349F95ADEBE05F0A71B5F5B62D0F696667454DA60CD55282F67321710C",
+                    "marked_inactive_on" : "2014-12-16 20:21:42"
+                }]'''
+            )
+
             response.status_code = 200
             mock_request.return_value = response
 
             airship = ua.Airship("key", "secret")
-            feedback = ua.Feedback.device_token(airship, datetime.datetime(2014,11,22))
+            feedback = ua.Feedback.device_token(
+                airship,
+                datetime.datetime(2014,11,22)
+            )
             date = '2014-12-16 20:21:42'
             try:
                 from dateutil.parser import parse
@@ -98,28 +104,43 @@ class TestDeviceInfo(unittest.TestCase):
                 def parse(date):
                     return date
 
-            self.assertEqual(feedback,[{u'device_token': u'4D95CB349F95ADEBE05F0A71B5F5B62D0F696667454DA60CD55282F67321710C',
-                                        u'alias': None,
-                                        u'marked_inactive_on': date}
-                                       ])
-            mock_request.assert_called_with('GET', '', 'https://go.urbanairship.com/api/device_tokens/feedback/',
-                                            version=3, params={'since': '2014-11-22T00:00:00'}
-                                            )
+            self.assertEqual(
+                feedback,
+                [
+                    {
+                        u'device_token': u'4D95CB349F95ADEBE05F0A71B5F5B62D0F696667454DA60CD55282F67321710C',
+                        u'alias': None,
+                        u'marked_inactive_on': date
+                    }
+                ]
+            )
+            mock_request.assert_called_with(
+                'GET',
+                '',
+                'https://go.urbanairship.com/api/device_tokens/feedback/',
+                version=3,
+                params={'since': '2014-11-22T00:00:00'}
+            )
 
     def test_apid_feedback(self):
         with mock.patch.object(ua.Airship, "_request") as mock_request:
             response = requests.Response()
-            response._content = ('''[{
-                                   "alias" : null,
-                                   "apid" : "80242a4c-d870-4fce-b0c3-724c865cfbfe",
-                                   "gcm_registration_id": "null",
-                                   "marked_inactive_on" : "2014-12-16 20:21:42"
-                                 }]''')
+            response._content = (
+                '''[{
+                    "alias" : null,
+                    "apid" : "80242a4c-d870-4fce-b0c3-724c865cfbfe",
+                    "gcm_registration_id": "null",
+                    "marked_inactive_on" : "2014-12-16 20:21:42"
+                }]'''
+            )
             response.status_code = 200
             mock_request.return_value = response
 
             airship = ua.Airship("key", "secret")
-            feedback = ua.Feedback.apid(airship, datetime.datetime(2014, 11, 22, 10, 10, 10))
+            feedback = ua.Feedback.apid(
+                airship,
+                datetime.datetime(2014, 11, 22, 10, 10, 10)
+            )
             date = '2014-12-16 20:21:42'
             try:
                 from dateutil.parser import parse
@@ -128,11 +149,21 @@ class TestDeviceInfo(unittest.TestCase):
                 def parse(date):
                     return date
 
-            self.assertEqual(feedback,[{u'apid': u'80242a4c-d870-4fce-b0c3-724c865cfbfe',
-                                        u'alias': None,
-                                        u'marked_inactive_on': date,
-                                        u'gcm_registration_id': u'null'}
-                                       ])
-            mock_request.assert_called_with('GET', '', 'https://go.urbanairship.com/api/apids/feedback/',
-                                            version=3, params={'since': '2014-11-22T10:10:10'}
-                                            )
+            self.assertEqual(
+                feedback,
+                [
+                    {
+                        u'apid': u'80242a4c-d870-4fce-b0c3-724c865cfbfe',
+                        u'alias': None,
+                        u'marked_inactive_on': date,
+                        u'gcm_registration_id': u'null'
+                    }
+                ]
+            )
+            mock_request.assert_called_with(
+                'GET',
+                '',
+                'https://go.urbanairship.com/api/apids/feedback/',
+                version=3,
+                params={'since': '2014-11-22T10:10:10'}
+            )
