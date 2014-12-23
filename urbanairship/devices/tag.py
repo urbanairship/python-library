eecfd7470f4cad46a20aecfb174eee7932476bb4
amanda houle <amanda.houle@urbanairship.com>
2014-12-22 18:38:09 -0800

    modified class name

--- urbanairship/devices/tag.py
+++ urbanairship/devices/tag.py
@@ -1,27 +1,64 @@
 from urbanairship import common
 
-class CreateTag(object):
+class Tag(object):
 	"""
     Optional call to create a tag not associated with any device.
 	
 	"""
 
+    def createTag(self):
+        if not self.url:
+            raise ValueError(
+                "Cannot create a tag without url.")
+        body = json.dumps(self.payload)
+        response = self._airship._request('PUT', body,
+            self.url, 'application/json', version=3)
+
+        data = response.json()
+        logger.info("Tag creation successful. tags_urls: %s",
+            ', '.join(data.get('tags_urls', [])))
+
+        return TagListResponse()   #arg?
+
+class TagListResponse(object):           ## I don't know if this class is necessary or in the right place!
+    """Respone 
+
+    Right now this is a fairly simple wrapper around the json payload response,
+    but making it an object gives us some flexibility to add functionality
+    later.
+
+    """
+    ok = None
+    tag_ids = None
+    tags_url = None
+    payload = None
+
+    def __init__(self, response):
+        data = response.json()
+        self.tag_ids = data.get('tags')
+        self.tags_url = data.get('tags_urls', [None])[0]
+        self.ok = data.get('ok')
+        self.payload = data
+
+    def __str__(self):
+        return "Response Payload: {0}".format(self.payload)
+
+
     
 
 
-class ListTag(DeviceList)
+class ListTag(DeviceList)     
     """
     Iterator for listing all of the tags for this application.
 
-    [Need to implement a limit to the # of tags that can come back.  Check w/Mele (don't have access to those docs?)]
-    :ivar limit: Number of entries to fetch in each page request.
-    :returns: Each "next" returns a :py:class:'????' object.
+    Will return the first 100 listings.
+    :returns: 
 
     """
 
-     start_url = common.CHANNEL_URL
-     data_attribute = 'tags'
-     id_key = 'tag'
+     start_url = common.TAGS_URL
+     data_attribute = 'tag'
+     id_key = 'tags'
 
      def next(self):
          try:
