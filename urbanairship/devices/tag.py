663f41b1330ebc0827e7ce872abf43bfb5d069ff
amanda houle <amanda.houle@urbanairship.com>
2014-12-23 17:15:05 -0800

    outline/structure of tag API

--- urbanairship/devices/tag.py
+++ urbanairship/devices/tag.py
@@ -2,77 +2,74 @@ from urbanairship import common
 
 class Tag(object):
 	"""
-    Optional call to create a tag not associated with any device.
-	
-	"""
-
-    def createTag(self):
-        if not self.url:
-            raise ValueError(
-                "Cannot create a tag without url.")
-        body = json.dumps(self.payload)
-        response = self._airship._request('PUT', body,
-            self.url, 'application/json', version=3)
-
-        data = response.json()
-        logger.info("Tag creation successful. tags_urls: %s",
-            ', '.join(data.get('tags_urls', [])))
-
-        return TagListResponse()   #arg?
-
-class TagListResponse(object):           ## I don't know if this class is necessary or in the right place!
-    """Respone 
-
-    Right now this is a fairly simple wrapper around the json payload response,
-    but making it an object gives us some flexibility to add functionality
-    later.
 
     """
-    ok = None
-    tag_ids = None
-    tags_url = None
-    payload = None
 
-    def __init__(self, response):
-        data = response.json()
-        self.tag_ids = data.get('tags')
-        self.tags_url = data.get('tags_urls', [None])[0]
-        self.ok = data.get('ok')
-        self.payload = data
+    params = {}
+    url = common.TAGS_URL + tag_name
+    response = airship._request('GET', None, url, version=3, params=params)
+    payload = response.json()
+    data_attribute = 'tags'
+    id_key = 'tags'
 
-    def __str__(self):
-        return "Response Payload: {0}".format(self.payload)
+    def __init__(self, airship, tag_name):
+   
+    def add(ios_channels=None, android_channels=None):
+        issue this api request
+    def remove(ios_channels=None, android_channels=None):
+        issue this api request
 
+tag = Tag(Airship, "high roller")
+tag.add(android_channels=['12345-1231231'])
+
+def tag_add(airship, tag_name, ios_channels, android_channels):
+    add some stuff here :)
+
+
+class TagList(DeviceList):    
+    """
+    Iterator for listing tags associated with this application.
+
+    Will return the first 100 listings only.
+
+    """
+
+    start_url = common.TAGS_URL
+    data_attribute = 'tags'
+    tag_name = 'tags'
+    params = {}
+    url = start_url + ????_id
+    response = airship._request('GET', None, url, version=3, params=params)
+    payload = response.json()
 
     
+class BatchTag():
+    def __init__(self, airship):
+        self.changelist = []
 
+    def addIOSChannel(channel, tags):
+        self.changelist.append({"ios_channel": channel, "tags": tags})
 
-class ListTag(DeviceList)     
-    """
-    Iterator for listing all of the tags for this application.
+    def apply():
+        issue api request
 
-    Will return the first 100 listings.
-    :returns: 
+#  Leave out Create a Tag
+ #   def createTag(self):
+ #    """Optional call to create a tag not associated with any device.
+    
+    # """
+ #        if not self.url:
+ #            raise ValueError(
+ #                "Cannot create a tag without url.")
+ #        body = json.dumps(self.payload)
+ #        response = self._airship._request('PUT', body,
+ #            self.url, 'application/json', version=3)
 
-    """
+ #        data = response.json()
+ #        logger.info("Tag creation successful. tags_urls: %s",
+ #            ', '.join(data.get('tags_urls', [])))
 
-     start_url = common.TAGS_URL
-     data_attribute = 'tag'
-     id_key = 'tags'
+ #        return TagListResponse(?) 
 
-     def next(self):
-         try:
-             return ChannelInfo.from_payload(self._token_iter.next(), self.id_key)
-         except StopIteration:
-             self._fetch_next_page()
-             return ChannelInfo.from_payload(self._token_iter.next(), self.id_key)
-
-
-#class AddTag(object)
-#    """
-#    
-#    """
-
-
-#class RemoveTag(object)
-#class BatchTag(object)
+   
+    
