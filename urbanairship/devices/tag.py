37be2b1f4f7d720de413b2ff9a23624e8eadc048
amanda houle <amanda.houle@urbanairship.com>
2014-12-31 17:40:01 -0800

    update changing structure yay

--- urbanairship/devices/tag.py
+++ urbanairship/devices/tag.py
@@ -7,8 +7,7 @@ from urbanairship import core
 
 logger = logging.getLogger('urbanairship')
 
-
-class TagList(object):
+class TagList():
     """Iterator for listing tags associated with this application.
 
     Will return the first 100 listings only.
@@ -16,105 +15,121 @@ class TagList(object):
 
     """
 
-    url = common.TAGS_URL
-    data_attribute = 'tags'
-    tag_name = 'tags'
-#    params = {}
+    def __init__(self, airship, tag_name):
+        self._airship = airship
+        self._airship.secret = secret
+        self._airship.key = key
+        self.tag_name = tag_name
+        self.url = common.TAGS_URL
+        self.data_attribute = 'tags' 
 
-    response = self.airship._request('GET', None, url, version=3)  #error here regarding 'airship'
-#    return response.json()
-#    payload = response.json()
+        response = airship._request('GET', None, url, version=3)
+        return response.json()
 
-    
-# #  **********************Leave out Tag Creation*******
-#  #   def createTag(self):
-#  #    """Optional call to create a tag not associated with any device.
-    
-#     # """
-#  #        if not self.url:
-#  #            raise ValueError(
-#  #                "Cannot create a tag without url.")
-#  #        body = json.dumps(self.payload)
-#  #        response = self._airship._request('PUT', body,
-#  #            self.url, 'application/json', version=3)
 
-#  #        data = response.json()
-#  #        logger.info("Tag creation successful. tags_urls: %s",
-#  #            ', '.join(data.get('tags_urls', [])))
-
-#  #        return TagListResponse(?) 
-
-   
 class Tag(object):
     """Adding and Removing Devices from a Tag.
-       Adding and Removing Tags from a Device.
 
     """
 
     def __init__(self, airship, tag_name):
         self._airship = airship
-        self.tag_name = tag_name
-        self.ios_channel = None
-        self.android_channel = None
-        self.amazon_channel = None
+        self.url = common.TAGS_URL + '/' + tag_name
         self.payload = None
    
 
     def add(self, ios_channels=None, android_channels=None, amazon_channels=None):
-        """Add device to a tag.
+        # do a check to see if empty or not
+        #if not empty, append
+        #if empty, add
 
-        """
-
-
-        payload = {}
-        if ios_channels is not None:
-            payload['ios_channels'] = {'add': ios_channels}
-        if android_channels is not None:
-            payload['android_channels'] = {'add': android_channels}
-        if amazon_channels is not None:
-            payload['amazon_channels'] = {'add': amazon_channels}
-        if not payload:
-            raise ValueError('Cannot add a tag without a channel_id.')
-        
-
-        body = json.dumps(self.payload)
-        response = self._airship._request(Airship, 'POST', body,
-            common.TAGS_URL + '/' + self.tag_name, 'application/json', version=3)
-
-        #data = response.json()
-        #logger.info("Successful tag added to device channel. tag_name: %s",
-        #     ', '.join(tag_name))
-   
-
-    def remove(self, ios_channels=None, android_channels=None):
-        """Remove device from a tag.
-
-        """
-
-
-        payload = {}
-        if ios_channels is not None:
-            payload['ios_channels'] = {'remove': ios_channels}
-        if android_channels is not None:
-            payload['android_channels'] = {'remove': android_channels}
-        if amazon_channels is not None:
-            payload['amazon_channels'] = {'remove': amazon_channels}
+        if isinstance(ios_channels):
+            ios_channels = [ios_channels]
+        if ios_channels not in self.payload:
+            self.payload['ios_channels'] = {'add': [self.ios_channels]}
         else:
+            if 'add' in self.payload['ios_channels']:
+                self.payload[ios_channels]['add'].extend(ios_channels)
+            else:
+                self.payload['ios_channels']['add'] = [ios_channels]
+        if isinstance(android_channels):
+            android_channels = [android_channels]
+        if android_channels not in self.payload:
+            self.payload['android_channels'] = {'add': [self.android_channels]}
+        else:
+            if 'add' in self.payload['android_channels']:
+                self.payload[android_channels]['add'].extend(android_channels)
+            else:
+                self.payload['android_channels']['add'] = [android_channels]
+        if isinstance(amazon_channels):
+            amazon_channels = [amazon_channels]
+        if amazon_channels not in self.payload:
+            self.payload['amazon_channels'] = {'add': [self.amazon_channels]}
+        else:
+            if 'add' in self.payload['amazon_channels']:
+                self.payload[amazon_channels]['add'].extend(amazon_channels)
+            else:
+                self.payload['amazon_channels']['add'] = [amazon_channels]
+        if not data:
+            raise ValueError('Cannot add a tag without a channel_id.')
+        return payload
+
+
+    def remove(self, ios_channels=None, android_channels=None, amazon_channels=None):
+        # do a check to see if empty or not
+        #if not empty, append
+        #if empty, add
+
+        if isinstance(ios_channels):
+            ios_channels = [ios_channels]
+        if ios_channels not in self.payload:
+            self.payload['ios_channels'] = {'remove': [self.ios_channels]}
+        else:
+            if 'remove' in self.payload['ios_channels']:
+                self.payload[ios_channels]['remove'].extend(ios_channels)
+            else:
+                self.payload['ios_channels']['remove'] = [ios_channels]
+        if isinstance(android_channels):
+            android_channels = [android_channels]
+        if android_channels not in self.payload:
+            self.payload['android_channels'] = {'remove': [self.android_channels]}
+        else:
+            if 'remove' in self.payload['android_channels']:
+                self.payload[android_channels]['remove'].extend(android_channels)
+            else:
+                self.payload['android_channels']['add'] = [android_channels]
+        if isinstance(amazon_channels):
+            amazon_channels = [amazon_channels]
+        if amazon_channels not in self.payload:
+            self.payload['amazon_channels'] = {'remove': [self.amazon_channels]}
+        else:
+            if 'add' in self.payload['amazon_channels']:
+                self.payload[amazon_channels]['remove'].extend(amazon_channels)
+            else:
+                self.payload['amazon_channels']['remove'] = [amazon_channels]
+        if not data:
             raise ValueError('Cannot remove a tag without a channel_id.')
-        
-
-        body = json.dumps(self.payload)
-        response = self._airship._request('POST', body,
-            common.TAGS_URL + '/' + self.tag_name, 'application/json', version=3)
-
-        #data = response.json()
-        #logger.info("Successful, tag removed.")
+        return payload
 
 
-    # def tag_add(airship, tag_name, ios_channels, android_channels, amazon_channels):
-    #     """
+    def apply(self):
+        """Issues POST request for adding / removing device(s) to / from a tag.
 
-    #     """
+        """
+
+        body = json.dumps(self.data)
+        response = self._airship._request(Airship, 'POST', body,
+            self.url, 'application/json', version=3)
+
+        data = response.json()
+        logger.info("Successful tag added to device channel. tag_name: %s",
+             ', '.join(tag_name))
+
+
+# """ 
+# Test:  tag = Tag(Airship, "high roller")
+#        tag.add_remove(android_channels={"add": ['9c36e8c7-5a73-47c0-9716-99fd3d4197d5']})
+# """
 
 class DeleteTag(object):
     """Delete Tag from the System.
@@ -127,14 +142,12 @@ class DeleteTag(object):
         self._airship = airship
         self.tag_name = tag_name
         self.url = common.TAGS_URL + '/' + tag_name
-        self.payload = None
-        # data_attribute = 'tags'
-        # tag_name = 'tags'
-        # params = {}
+        self._airship.secret = secret
+        self._airship.key = key
 
 
-    response = self.airship._request('DELETE', None, url, version=3)
-    return response
+        response = self.airship._request('DELETE', None, url, version=3)
+        return response
 
 
 # class BatchTag():
