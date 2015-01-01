58553d2eabf465754c80e46f7660be280d5c9571
amanda houle <amanda.houle@urbanairship.com>
2014-12-31 17:39:35 -0800

    update tests adding mock

--- tests/devices/test_tags.py
+++ tests/devices/test_tags.py
@@ -5,68 +5,77 @@ import urbanairship as ua
 
 class TestTags(unittest.TestCase):
 
-    def test__tag_listing(self):
-            {
-                "tags": []
-            }
-    def test_create_tag_success(self):
-# def test_push_success(self):
-#         with mock.patch.object(ua.Airship, '_request') as mock_request:
-#             response = requests.Response()
-#             response._content = (
-#                 '''{"push_ids": ["0492662a-1b52-4343-a1f9-c6b0c72931c0"]}''')
-#             response.status_code = 202
-#             mock_request.return_value = response
+    def test__tag_list(self):
+        my_app_tag_list = TagList(airship)
+self.assertEqual(tags, {
+            "tags": [
+               "some_tag", 
+               "portland_or",
+               "another_tag"
+            ]
+      })
 
-#             airship = ua.Airship('key', 'secret')
-#             push = airship.create_push()
-#             push.audience = ua.all_
-#             push.notification = ua.notification(alert='Hello')
-#             push.options = ua.options(expiry=10080)
-#             push.device_types = ua.all_
-#             pr = push.send()
-#             self.assertEqual(
-#                 pr.push_ids, ['0492662a-1b52-4343-a1f9-c6b0c72931c0'])
+def test_add_device(self):
+    with mock.patch.object(ua.Airship, '_request') as mock_request:
+        response = requests.Response()
+        response._content = (
+            '''{"ios_channels": ["0492662a-1b52-4343-a1f9-c6b0c72931c0"]}''')
+        response.status_code = 202
+        mock_request.return_value = response
 
-    def test_add_tag_iOS(self):
-    def test_add_tag_Android(self):
-    def test_add_tag_Amazon(self):
-    	# d = datetime.datetime(2015, 1, 1, 12, 56)
-    	# self.assertEqual(ua.local_scheduled_time(d),
-    	# 	{'local_scheduled_time': '2015-01-01T12:56:00'})
+        airship = ua.Airship('key', 'secret')
+        tag = tag.add()
+        tag.payload = {}     # pick up here on Friday!
+        pr = tag.apply()
+        self.assertEqual(
+            pr.tags, 'OK')
 
-    def test_remove_tag_iOS(self):
-    def test_remove_tag_Android(self):
-    def test_remove_tag_Amazon(self):
+#     def test_add_device(self):              
+#         tag = Tag(Airship, "high roller")
+#         tag.add(ios_channels=['9c36e8c7-5a73-47c0-9716-99fd3d4197d5'])
+#         tag.remove(android_channels=['channel_to_remove'])
+#         self.assertEqual(ios_channels=['9c36e8c7-5a73-47c0-9716-99fd3d4197d5'], {  #?
+#             "ios_channels": {
+#                 "add": [
+#                     "9c36e8c7-5a73-47c0-9716-99fd3d4197d5",
+#                     "9c36e8c7-5a73-47c0-9716-99fd3d4197d6"
+#                     ]
+#             },
+#             "android_channels": {
+#                 "remove": [
+#                     "channel_to_remove"
+#                 ]
+#             } 
+#         })
 
-    def test_batch_tag_iOS(self):
-    def test_batch_tag_Android(self):
-    def test_batch_tag_Amazon(self):
+#     def test_delete_tag(self):
 
-push = airship.create_push()
-   push.audience = ua.and_(
-      ua.tag("breakingnews"),
-      ua.or_(
-         ua.tag("sports"),
-         ua.tag("worldnews")
-      )
-   )
-   push.notification = ua.notification(
-      ios=ua.ios(
-         alert="Kim Jong-Un wins U.S. Open",
-         badge="+1",
-         extra={"articleid": "123456"}
-      ),
-      android=ua.android(
-         alert="Breaking Special Android News! Glorious Leader Kim Jong-Un wins U.S. Open!",
-         extra={"articleid": "http://m.example.com/123456"}
-      ),
-      amazon=ua.amazon(
-      alert='Breaking Amazon News!',
-      expires_after=60,
-      extra={'articleid': '12345'},
-      summary='This is a short message summary',
-      )
-   )
-   push.device_types = ua.device_types('ios', 'android', 'amazon')
-   push.send()
+#     def test_batch_tag(self):
+
+# push = airship.create_push()
+#    push.audience = ua.and_(
+#       ua.tag("breakingnews"),
+#       ua.or_(
+#          ua.tag("sports"),
+#          ua.tag("worldnews")
+#       )
+#    )
+#    push.notification = ua.notification(
+#       ios=ua.ios(
+#          alert="Kim Jong-Un wins U.S. Open",
+#          badge="+1",
+#          extra={"articleid": "123456"}
+#       ),
+#       android=ua.android(
+#          alert="Breaking Special Android News! Glorious Leader Kim Jong-Un wins U.S. Open!",
+#          extra={"articleid": "http://m.example.com/123456"}
+#       ),
+#       amazon=ua.amazon(
+#       alert='Breaking Amazon News!',
+#       expires_after=60,
+#       extra={'articleid': '12345'},
+#       summary='This is a short message summary',
+#       )
+#    )
+#    push.device_types = ua.device_types('ios', 'android', 'amazon')
+#    push.send()
