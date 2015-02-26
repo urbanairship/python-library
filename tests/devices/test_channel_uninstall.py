d3474fa8409dc2a8f4933ae590a8d238a101a88f
Max Marchuk <maxmarchuk@gmail.com>
2015-02-26 11:28:50 -0800

    Added channel uninstall test and example

--- tests/devices/test_channel_uninstall.py
+++ tests/devices/test_channel_uninstall.py
@@ -9,23 +9,15 @@ class TestChannelUninstall(unittest.TestCase):
     def test_channel_uninstall(self):
         with mock.patch.object(ua.Airship, '_request') as mock_request:
             response = requests.Response()
-            response._content = ('{"ok": true}')
+            response._content = '{"ok": true}'.encode('utf-8')
             mock_request.return_value = response
             airship = ua.Airship('key', 'secret')
 
             cu = ua.ChannelUninstall(airship)
 
-            chans = [
-                {
-                    "channel_id": "01000001-01010000-01010000-01001100",
-                    "device_type": "ios"
-                },
-                {
-                    "channel_id": "01000111-01001111-01001111-01000111",
-                    "device_type": "android"
-                }
-            ]
+            chans = [{"channel_id": "01000001-01010000-01010000-01001100",
+                      "device_type": "ios"}]
 
-            cu_response = json.loads(cu.uninstall(chans).content)
-            print cu_response
-            self.assertEqual(cu_response["ok"], True)
+            cu_res = json.loads(cu.uninstall(chans).content.encode('utf-8'))
+
+            self.assertEqual(cu_res["ok"], True)
