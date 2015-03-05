d20a91ec9040dcb9aad460fea84dbf8d336f25df
Max Marchuk <maxmarchuk@gmail.com>
2015-03-05 13:25:42 -0800

    removed unnecessary variable

--- urbanairship/devices/channel_uninstall.py
+++ urbanairship/devices/channel_uninstall.py
@@ -14,15 +14,14 @@ class ChannelUninstall(object):
         self._airship = airship
 
     def uninstall(self, channels):
-        body = json.dumps(channels)
-        chan_dict = channels
-        chan_num = len(chan_dict)
+        chan_num = len(channels)
 
         if chan_num > 200:
             print("Maximum of 200 channel uninstalls exceeded. ({} channels)"
                   .format(chan_num))
             return
 
+        body = json.dumps(channels)
         response = self._airship._request('POST', body, self.url, version=3)
         logger.info("Successfully uninstalled {} channels".format(chan_num))
         return response
