9a385007b5a48513845a28faa745d3db09b87e67
Max Marchuk <maxmarchuk@gmail.com>
2015-05-01 11:18:08 -0700

    formatting

--- urbanairship/reports/per_push.py
+++ urbanairship/reports/per_push.py
@@ -12,10 +12,10 @@ class Detail(object):
 
     def get_single(self, push_id):
         if not push_id:
-            raise ValueError("'push_id' cannot be empty")
+            raise ValueError('push_id cannot be empty')
 
         if not isinstance(push_id, str):
-            raise TypeError("'push_id' must be a string")
+            raise TypeError('push_id must be a string')
         response = self.airship._request('GET', None, common.REPORTS_URL +
                                          'perpush/detail/' + push_id,
                                          version=3)
@@ -23,11 +23,11 @@ class Detail(object):
 
     def get_batch(self, push_ids):
         if not push_ids or push_ids is []:
-            raise ValueError("'push_ids' cannot be empty")
+            raise ValueError('push_ids cannot be empty')
         if not isinstance(push_ids, list):
-            raise TypeError("'push_ids' must be a list")
+            raise TypeError('push_ids must be a list')
         if len(push_ids) > 100:
-            raise ValueError("'push_ids' can not contain more than 100 IDs")
+            raise ValueError('push_ids can not contain more than 100 IDs')
 
         data = {}
         data['push_ids'] = push_ids
@@ -46,7 +46,7 @@ class Series(object):
 
     def get(self, push_id):
         if not isinstance(push_id, str):
-            raise TypeError("'push_id' must be a string")
+            raise TypeError('push_id must be a string')
 
         url = common.REPORTS_URL + 'perpush/series/{0}'.format(push_id)
         response = self.airship._request('GET', None, url, version=3)
@@ -54,10 +54,9 @@ class Series(object):
 
     def get_with_precision(self, push_id, precision):
         if not isinstance(push_id, str):
-            raise TypeError("'push_id' must be a string")
+            raise TypeError('push_id must be a string')
         if precision not in ['HOURLY', 'DAILY', 'MONTHLY']:
-            raise ValueError(
-                "Precision must be 'HOURLY', 'DAILY', or 'MONTHLY'")
+            raise ValueError("Precision must be 'HOURLY', 'DAILY', or 'MONTHLY'")
 
         url = common.REPORTS_URL + 'perpush/series/{0}?precision={1}'.format(
             push_id, precision)
@@ -68,7 +67,7 @@ class Series(object):
     def get_with_precision_and_range(self, push_id, precision, start,
                                      end):
         if not isinstance(push_id, str):
-            raise TypeError("'push_id' must be a string")
+            raise TypeError('push_id must be a string')
         if precision not in ['HOURLY', 'DAILY', 'MONTHLY']:
             raise ValueError(
                 "Precision must be 'HOURLY', 'DAILY', or 'MONTHLY'")
