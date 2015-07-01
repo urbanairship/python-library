095636c7fd6485769d561ca47ce4184340cd61c1
kristinafrye <kristina.frye@urbanairship.com>
2015-07-01 14:55:56 -0700

    Minor changes in response to PR comments

--- tests/reports/test_response_statistics.py
+++ tests/reports/test_response_statistics.py
@@ -6,11 +6,11 @@ import json
 import urbanairship as ua
 
 class TestResponseStatistics(unittest.TestCase):
-    def test_Response_Statistics(self):
+    def test_response_statistics(self):
         mock_response = requests.Response()
         mock_response._content = json.dumps({
             "push_uuid": "f133a7c8-d750-11e1-a6cf-e06995b6c872",
-            "direct_responses": "45",
+            "direct_responses": 45,
             "sends": 123,
             "push_type": "UNICAST_PUSH",
             "push_time": "2012-07-31 12:34:56"
@@ -20,10 +20,10 @@ class TestResponseStatistics(unittest.TestCase):
         ua.Airship._request.side_effect = [ mock_response ]
 
         airship = ua.Airship('key', 'secret')
-        statistics = ua.IndividualResponseStats(airship).get('push_id')
+        statistics = ua.reports.IndividualResponseStats(airship).get('push_id')
 
         self.assertEqual(statistics['push_uuid'], "f133a7c8-d750-11e1-a6cf-e06995b6c872")
-        self.assertEqual(statistics['direct_responses'], "45")
+        self.assertEqual(statistics['direct_responses'], 45)
         self.assertEqual(statistics['sends'], 123)
         self.assertEqual(statistics['push_type'], "UNICAST_PUSH")
         self.assertEqual(statistics['push_time'], "2012-07-31 12:34:56")
