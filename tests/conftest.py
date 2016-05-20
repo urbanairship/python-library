2ca9ed9d1b48d2ff113928f9050e5d0b9282bff5
amanda houle <amanda.gft@gmail.com>
2016-05-20 10:44:33 -0700

    clean slate

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-"""
-    Configuration of command line options and other test parameters
-"""
-
-def pytest_addoption(parser):
-    parser.addoption("--host", action="store", default="local", help="Host is local, stag or prod")
-    parser.addoption("--api_key", action="store", default=None, help="Your API key")
