6d02a75695e4cb5439fa5956f724a24ed36b73f8
Devin Smythe <devin.smythe@urbanairship.com>
2016-09-02 15:15:09 -0700

    Fixed indenting and updated new keyword descriptions used for the ios argument docs

--- urbanairship/push/payload.py
+++ urbanairship/push/payload.py
@@ -76,15 +76,16 @@ def ios(alert=None, badge=None, sound=None, content_available=False,
     :keyword title: Sets the title of the notification for Apple Watch. Must
         be a string
     :keyword mutable_content: Optional, a boolean, one of true or false. Defaults to false.
-    When set to true, content may be modified by an extension.
-    This flag will be automatically set to true if there is a media_attachment in the payload. iOS 10 or above.
+        When set to true, content may be modified by an extension.
+        This flag will be automatically set to true if there is a media_attachment in the payload. iOS 10 or above.
     :keyword collapse_id: Optional, a string. When there is a newer message that renders an older,
-    related message irrelevant to the client app, the new message replaces the older message with the same collapse_id.
-    Similar to the GCM collapse_key. iOS 10 or above.
+        related message irrelevant to the client app, the new message replaces the older message with the same collapse_id.
+        Similar to the GCM collapse_key. iOS 10 or above.
     :keyword subtitle: Optional, a string that will display below the title of the notification.
-    This is provided as a convenience for setting the subtitle in the alert JSON object.
-    If a subtitle is also defined in the alert JSON object, this value is ignored. iOS 10 or above.
-    :keyword media_attachment:
+        This is provided as a convenience for setting the subtitle in the alert JSON object.
+        If a subtitle is also defined in the alert JSON object, this value is ignored. iOS 10 or above.
+    :keyword media_attachment: Optional, a dictionary object `Media Attachment <https://docs.urbanairship.com/api/ua.html#media-attachment>`_. Specifies a media attachment
+        to be handled by the UA Media Attachment Extension.
 
     >>> ios(alert='Hello!', sound='cat.caf',
     ...     extra={'articleid': '12345'}) # doctest: +SKIP
