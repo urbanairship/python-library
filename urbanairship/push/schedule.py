22f3d463819fa0717f4ce6100cc4d34558b1da50
Aaron Schuman <aaron.schuman@urbanairship.com>
2018-04-17 09:39:41 -0700

    ScheduledList class added

--- urbanairship/push/schedule.py
+++ urbanairship/push/schedule.py
@@ -1,3 +1,24 @@
+from urbanairship import common
+from urbanairship.push import ScheduledPush
+
+
+class ScheduledList(common.IteratorParent):
+    """
+    Iterator for listing all scheduled push for an engage project.
+
+    :ivar limit: Number of entries to fetch in a paginated request.
+    :returns Each ``next`` returns a :py:class:`ScheduledPush` object.
+    """
+    next_url = common.SCHEDULES_URL
+    data_attribute = 'schedules'
+    id_key = 'url'
+    instance_class = ScheduledPush
+
+    def __init__(self, airship, limit=None):
+        params = {'limit': limit} if limit else {}
+        super(ScheduledList, self).__init__(airship, params)
+
+
 def scheduled_time(timestamp):
     """Specify a time for the delivery of this push.
 
