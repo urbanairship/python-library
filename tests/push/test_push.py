774bfc29cfaa91cf68a47227bd59856b09ae543e
Aaron Schuman <aaron.schuman@urbanairship.com>
2018-09-18 15:47:01 -0400

    add ios override tests

--- tests/push/test_push.py
+++ tests/push/test_push.py
@@ -357,12 +357,26 @@ class TestPush(unittest.TestCase):
             }
         )
 
-    def test_ios_alert_dict(self):
+    def test_ios_overrides(self):
         p = ua.Push(None)
         p.audience = ua.all_
         p.notification = ua.notification(
             ios=ua.ios(
-                alert={'foo': 'bar'}
+                alert={'title': 'this is',
+                       'body': 'backwards',
+                       'summary-arg': 'Matmos',
+                       'summary-arg-count': 1},
+                sound={'name': 'Amplified Synapse',
+                       'volume': 0.8,
+                       'critical': False},
+                thread_id='plastic minor',
+                priority=10,
+                badge=3,
+                extra={'office': 'furniture'},
+                mutable_content=False,
+                title='this is',
+                subtitle='backwards',
+                collapse_id='nugent sand'
             )
         )
         p.options = ua.options(10080)
@@ -381,8 +395,26 @@ class TestPush(unittest.TestCase):
                 'notification': {
                     'ios': {
                         'alert': {
-                            'foo': 'bar'
-                        }
+                            'title': 'this is',
+                            'body': 'backwards',
+                            'summary-arg': 'Matmos',
+                            'summary-arg-count': 1
+                        },
+                        'sound': {
+                            'name': 'Amplified Synapse',
+                            'volume': 0.8,
+                            'critical': False
+                        },
+                        'thread_id': 'plastic minor',
+                        'priority': 10,
+                        'badge': 3,
+                        'extra': {
+                            'office': 'furniture'
+                        },
+                        'mutable_content': False,
+                        'title': 'this is',
+                        'subtitle': 'backwards',
+                        'collapse_id': 'nugent sand'
                     }
                 },
                 'device_types': 'ios',
