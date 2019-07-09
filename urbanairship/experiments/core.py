64a7318ad0a410d2cb5edbdad73ad5b736de87f6
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2019-07-09 13:48:41 -0700

    crete experiment orject with stub methods

---
+++ urbanairship/experiments/core.py
@@ -0,0 +1,19 @@
+class Experiment(object):
+
+    def list_experiments(self):
+        pass
+
+    def create(self):
+        pass
+
+    def list_scheduled_experiment(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def validate(self):
+        pass
+
+    def lookup(self):
+        pass
