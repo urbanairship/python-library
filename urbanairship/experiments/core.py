a5c5796ce96c0c7e02b510a9ebcd8887bc667d73
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2019-07-11 11:40:23 -0700

    check and build response body

--- urbanairship/experiments/core.py
+++ urbanairship/experiments/core.py
@@ -1,7 +1,29 @@
+from urbanairship import common
+
+
 class Experiment(object):
 
-    def list_experiments(self):
-        pass
+    def list_experiments(self, limit=None):
+        """List experiments
+
+        :keyword limit: Optional, maximum number of experiments, 
+        default is 10, max is 100
+        """
+
+        params = {}
+        if isinstance(limit, int):
+            params['limit'] = limit
+
+        url = common.EXPERIMENTS_URL
+        response = self.airship.request(
+            method='GET',
+            body=None,
+            params=params,
+            url=url,
+            version=3
+        )
+   
+        return response
 
     def create(self):
         pass
