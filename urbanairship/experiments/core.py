1960021198cabb09da21d5027a56f9596f30c260
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2019-07-15 15:18:11 -0700

    made new function to get listings

--- urbanairship/experiments/core.py
+++ urbanairship/experiments/core.py
@@ -3,18 +3,17 @@ from urbanairship import common
 
 class Experiment(object):
 
-    def list_experiments(self, limit=None):
+    def _get_listing(self, url, limit=None):
         """List experiments
 
-        :keyword limit: Optional, maximum number of experiments, 
+        :keyword limit: Optional, maximum number of experiments,
         default is 10, max is 100
         """
 
         params = {}
         if isinstance(limit, int):
             params['limit'] = limit
-
-        url = common.EXPERIMENTS_URL
+    
         response = self.airship.request(
             method='GET',
             body=None,
@@ -22,14 +21,18 @@ class Experiment(object):
             url=url,
             version=3
         )
-   
         return response
 
+    def list_experiments(self, _body_builder_):
+        url = common.EXPERIMENTS_URL
+        return self._get_listing(url)
+ 
     def create(self):
         pass
 
     def list_scheduled_experiment(self):
-        pass
+        url = common.EXPERIMENTS_SCHEDULE_URL
+        return self._get_listing(url)
 
     def delete(self):
         pass
