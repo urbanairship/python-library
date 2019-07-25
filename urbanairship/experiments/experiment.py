5877896bbc6b7d690cd75b8ca6163effa4f8e5d5
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2019-07-25 14:49:50 -0700

    add description, campaings,audiance and control

--- urbanairship/experiments/experiment.py
+++ urbanairship/experiments/experiment.py
@@ -4,11 +4,26 @@ class Experiment(object):
     """
 
     def __init__(self,
-                 name=None):
+                 audience,
+                 name=None,
+                 description=None,
+                 campaigns=None,
+                 control=None
+                 ):
         """
         :keyword name: [optional] A name for the experiment
+        :keyword description: [optional] A description of the experiment
+        :keyword campaigns: [optional] Campaigns object that will be applied to
+            resulting pushes
+        :keyword audience: [required] The audience for the experiment
+        :keyword control: [optional] The proportional subset of the audience that will
+             not receive a push
         """
         self.name = name
+        self.description = description
+        self.campaigns = campaigns
+        self.audience = audience
+        self.control = control
 
     @property
     def name(self):
@@ -24,3 +39,36 @@ class Experiment(object):
             )
 
         self._name = value
+
+    @property
+    def description(self):
+        if not self._description:
+            return None
+        return self._description
+
+    @description.setter
+    def description(self, value):
+        if not isinstance(value, str):
+            TypeError(
+                'the description must be type string'
+            )
+
+        self._description = value
+
+    @property
+    def control(self):
+        if not self._control:
+            return None
+        return self._control
+
+    @control.setter
+    def control(self, value):
+        if not isinstance(value, float):
+            TypeError(
+                'the control must be type float'
+            )
+        if not 0.0 >= value >= 1.0:
+            ValueError(
+                'control must be in a range of 0.0 and 1.0'
+            )
+        self._control = value
