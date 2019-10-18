88f47a804ad8f5519d7af116e41aa315f687c239
Nadiya Yeroshkina <nadin.yeroshkina@gmail.com>
2019-10-18 15:00:20 -0700

    add delete and create set up

--- urbanairship/experiments/core.py
+++ urbanairship/experiments/core.py
@@ -1,7 +1,10 @@
 from urbanairship import common
+import json
 
 
 class AB_test(object):
+    def __ini__(self, airship):
+        self.airship = airship
 
     def _get_listing(self, url, limit=None):
         """List experiments
@@ -27,15 +30,39 @@ class AB_test(object):
         url = common.EXPERIMENTS_URL
         return self._get_listing(url)
 
-    def create(self):
-        pass
+    def create(self, experiment):
+        """ Create an experiment """
 
+        url = common.EXPERIMENTS_URL
+        body = json.dumps(experiment.payload)
+        response = self.airship.request(
+            method='POST',
+            body=body,
+            url=url,
+            content_type='aplication/json',
+            version=3
+        )
+        return response
+        
     def list_scheduled_experiment(self):
         url = common.EXPERIMENTS_SCHEDULE_URL
         return self._get_listing(url)
 
-    def delete(self):
-        pass
+    def delete(self, experiment_id):
+        """ Delete a scheduled experiment. You can only delete experiments before they start
+        
+        :keyword experiment_id: The unique identifier of the experiment, type string
+        DELETE /api/experiments/scheduled/{experiment_id}
+        """
+        url = common.EXPERIMENTS_SCHEDULE_URL + experiment_id
+        response = self.airship.request(
+            method='DELETE',
+            body=None,
+            url=url,
+            version=3
+        )
+
+        return response
 
     def validate(self):
         pass
