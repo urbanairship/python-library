5b0bebd63da339ccd43c35408641f1d1af6fa2ab
whileloup <alexander.privee@urbanairship.com>
2021-07-16 16:22:40 -0700

    Created Tag_Group object in audience.py and edited __init__ files accordingly

--- urbanairship/push/audience.py
+++ urbanairship/push/audience.py
@@ -96,6 +96,10 @@ def tag(tag):
     """Select a single tag."""
     return {'tag': tag}
 
+def tag_group(tag_group, tag):
+    """Select a tag group and a tag."""
+    payload = {'group': tag_group, 'tag': tag}
+    return payload
 
 def alias(alias):
     """Select a single alias."""
